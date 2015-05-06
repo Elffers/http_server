0504214ac8fd76a0f7c3a6dade26caf5720e3162
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-05-05 20:46:51 -0700

    Increment request_number

--- mt_server.rb
+++ mt_server.rb
@@ -1,6 +1,9 @@
 require 'socket'
+require 'thread' # this is to get mutexes (mutual exclusion)
 
 server = TCPServer.new 8000 # Server bind to port 8000
+request_number = 0
+mutex = Mutex.new
 
 loop do
   client = server.accept
@@ -8,9 +11,12 @@ loop do
     request = client.gets("\r\n\r\n")
 
     response = "HTTP/1.1 200 OK\r\nconnection: close\r\ncontent-type: text/plain\r\n\r\n"
-    body = "It worked!\n"
 
-    sleep 5
+    current_request = mutex.synchronize do
+      request_number += 1
+    end
+
+    body = "#{current_request}\n"
 
     client.write response # client.write sends the actual response message
     client.write body
