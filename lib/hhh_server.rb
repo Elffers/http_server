5f83b3262c7c2b9566bdf9c416dccb4a66500bd8
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-08-19 21:01:47 -0700

    Wrap server in class

--- lib/hhh_server.rb
+++ lib/hhh_server.rb
@@ -1,49 +1,54 @@
 require 'socket'
 
-server = TCPServer.new 8000 # Server bind to port 8000
+class HHHServer
+  def start
 
-loop do
-  # server.accept is a blocking process, so the thread stops and waits until
-  # a client connects. That is, nothing else gets executed until a 3-way
-  # handshake is completed
+    server = TCPServer.new 8000 # Server bind to port 8000
 
-  client = server.accept
-  # the client is a TCPSocket object, which is a stream/connection
+    loop do
+      # server.accept is a blocking process, so the thread stops and waits until
+      # a client connects. That is, nothing else gets executed until a 3-way
+      # handshake is completed
 
-  # This only reads the request header, since body always comes after a newline
-  request = client.gets("\r\n\r\n")
+      client = server.accept
+      # the client is a TCPSocket object, which is a stream/connection
 
-  # Extracts the request uri
-  /[A-Z]+ (?<path>[^ ]+)/ =~ request
+      # This only reads the request header, since body always comes after a newline
+      request = client.gets("\r\n\r\n")
 
-  status = "HTTP/1.1 "
-  # tacks on path to current dir to request uri
-  path = Dir.pwd + path
+      # Extracts the request uri
+      /[A-Z]+ (?<path>[^ ]+)/ =~ request
 
-  # TODO: make sure path name is secure using Pathname
-  if File.directory? path
-    output = "<ul>"
-    Dir.entries(Dir.pwd).map do |file|
-      output += "<li><a href=\"/#{file}\">#{file}</a></li>"
+      status = "HTTP/1.1 "
+      # tacks on path to current dir to request uri
+      path = Dir.pwd + path
+
+      # TODO: make sure path name is secure using Pathname
+      if File.directory? path
+        output = "<ul>"
+        Dir.entries(Dir.pwd).map do |file|
+          output += "<li><a href=\"/#{file}\">#{file}</a></li>"
+        end
+        output += "</ul>\n"
+        body = output
+        status += "200 OK"
+        content_type = 'text/html'
+      elsif File.exists? path
+        body = File.read path
+        status += "200 OK"
+        content_type = 'text/plain'
+      else
+        body = "404 Not Found :("
+        status += "404 Not Found"
+        content_type = "text/plain"
+      end
+
+      response = status + "\r\nconnection: close\r\ncontent-type:" + content_type + "\r\n\r\n"
+
+      client.write response
+      client.write body
+
+      client.close
     end
-    output += "</ul>\n"
-    body = output
-    status += "200 OK"
-    content_type = 'text/html'
-  elsif File.exists? path
-    body = File.read path
-    status += "200 OK"
-    content_type = 'text/plain'
-  else
-    body = "404 Not Found :("
-    status += "404 Not Found"
-    content_type = "text/plain"
   end
-
-  response = status + "\r\nconnection: close\r\ncontent-type:" + content_type + "\r\n\r\n"
-
-  client.write response
-  client.write body
-
-  client.close
 end
